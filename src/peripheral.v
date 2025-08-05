b18bcaa1bee08d9f66861ff36304e6837f77e7dd
fjpolo <fjpolo@gmail.com>
2025-08-05 11:56:49 +0200

    Test vanilla peripheral.v

--- src/peripheral.v
+++ src/peripheral.v
@@ -5,231 +5,60 @@
 
 `default_nettype none
 
-module tqvp_fjpolo_rv2a03 (
-    input          clk,           // Clock - the TinyQV project clock is normally set to 64MHz.
-    input          rst_n,         // Reset_n - low to reset.
+// Change the name of this module to something that reflects its functionality and includes your name for uniqueness
+// For example tqvp_yourname_spi for an SPI peripheral.
+// Then edit tt_wrapper.v line 41 and change tqvp_example to your chosen module name.
+module tqvp_example (
+    input         clk,          // Clock - the TinyQV project clock is normally set to 64MHz.
+    input         rst_n,        // Reset_n - low to reset.
 
-    input    [7:0] ui_in,         // The input PMOD, always available. Note that ui_in[7] is normally used for UART RX.
-    output   [7:0] uo_out,        // The output PMOD. Each wire is only connected if this peripheral is selected.
+    input  [7:0]  ui_in,        // The input PMOD, always available.  Note that ui_in[7] is normally used for UART RX.
+                                // The inputs are synchronized to the clock, note this will introduce 2 cycles of delay on the inputs.
 
-    input    [5:0] address,       // Address within this peripheral's address space
-    input   [31:0] data_in,       // Data in to the peripheral, bottom 8, 16 or all 32 bits are valid on write.
+    output [7:0]  uo_out,       // The output PMOD.  Each wire is only connected if this peripheral is selected.
+                                // Note that uo_out[0] is normally used for UART TX.
 
-    input    [1:0] data_write_n,  // 11 = no write, 00 = 8-bits, 01 = 16-bits, 10 = 32-bits
-    input    [1:0] data_read_n,   // 11 = no read, 00 = 8-bits, 01 = 16-bits, 10 = 32-bits
+    input [5:0]   address,      // Address within this peripheral's address space
+    input [31:0]  data_in,      // Data in to the peripheral, bottom 8, 16 or all 32 bits are valid on write.
+
+    // Data read and write requests from the TinyQV core.
+    input [1:0]   data_write_n, // 11 = no write, 00 = 8-bits, 01 = 16-bits, 10 = 32-bits
+    input [1:0]   data_read_n,  // 11 = no read,  00 = 8-bits, 01 = 16-bits, 10 = 32-bits
     
-    output  [31:0] data_out,      // Data out from the peripheral
-    output         data_ready,
+    output [31:0] data_out,     // Data out from the peripheral, bottom 8, 16 or all 32 bits are valid on read when data_ready is high.
+    output        data_ready,
 
-    output         user_interrupt // Dedicated interrupt request for this peripheral
+    output        user_interrupt  // Dedicated interrupt request for this peripheral
 );
 
-    wire [7:0] apu_dout;
-    wire apu_o_ce; // New wire to capture the clock enable from the APU
-
-    localparam CONFIGURATION0_REG_ADDR = 6'h20;
-    localparam CONFIGURATION1_REG_ADDR = 6'h21;
-    localparam STATUS1_REG_ADDR = 6'h22;
-    localparam DATA_INPUT_REG_ADDR = 6'h23;
-    localparam DATA_OUTPUT_MSB_REG_ADDR = 6'h24;
-    localparam DATA_OUTPUT_LSB_REG_ADDR = 6'h25;
-    localparam APU_STATUS_REG_ADDRESS  = 6'h15;
-    localparam APU_FRAME_COUNTER_REG_ADDRESS  = 6'h17;
-
-    reg [7:0] reg_apu [30:0];
-    reg [7:0] reg_configuration0;
-    reg [7:0] reg_configuration1;
-    reg [7:0] reg_data_input;
-    reg [7:0] reg_data_output_msb;
-    reg [7:0] reg_data_output_lsb;
-    reg [7:0] reg_status0;
-
-    initial reg_configuration0 = 8'h00;      
-    initial reg_configuration1 = 8'h00;      
-    initial reg_data_input = 8'h00;          
-    initial reg_data_output_msb = 8'hFF;     
-    initial reg_data_output_lsb = 8'h00;     
-    initial reg_status0 = 8'h00;             
-
-    wire apu_us = reg_configuration0[2];
-    wire apu_enhanced = 1'b0;
-    wire apu_mapper_saturates = reg_configuration1[0];
-    wire apu_is_mmc5 = 1'b0;
-    wire [4:0] apu_audio_channels = reg_configuration1[6:2];
-
-    wire [7:0] apu_data_out;
-    wire [15:0] apu_output_sample_16b;
-    wire apu_data_output_ready;         
-    
-    wire apu_IRQ;
-    
-    reg odd_or_even = 1; 
-
-    parameter CPU_DIV_N = 4'd11; 
-    parameter PPU_DIV_N = 2'd3;  
-
-    reg [3:0] div_cpu_cnt;
-    initial div_cpu_cnt = 4'd0;
-    reg [1:0] div_ppu_cnt;
-    initial div_ppu_cnt = 2'd0;
-    reg [1:0] div_sys;
-    initial div_sys = 2'd0;
-
-    wire cpu_ce = (div_cpu_cnt == CPU_DIV_N);
-    wire ppu_ce = (div_ppu_cnt == PPU_DIV_N);
-    wire apu_ce = cpu_ce;
-    
-    // The derived clock is now only for the output pin.
-    wire apu_phi2_clk = (div_cpu_cnt >= 4'd4);
-    
-    // This is the new clock enable signal for the APU module.
-    wire apu_phi2_ce = apu_phi2_clk;
-
-    wire apu_cs = (address >= 'h00)&&(address < APU_FRAME_COUNTER_REG_ADDRESS);
-
-    // always @(posedge clk) begin
-    //     if (!rst_n) begin
-    //         div_cpu_cnt <= 4'd0;
-    //         div_ppu_cnt <= 2'd0;
-    //         div_sys     <= 2'd0;
-    //         odd_or_even <= 1'b1;
-    //     end else begin
-    //         div_cpu_cnt <= cpu_ce ? 4'd0 : div_cpu_cnt + 4'd1;
-    //         div_ppu_cnt <= ppu_ce ? 2'd0 : div_ppu_cnt + 2'd1;
-    //         div_sys     <= div_sys + 2'd1;
-            
-    //         if (cpu_ce) 
-    //             odd_or_even <= ~odd_or_even;
-    //     end
-    // end
-
-    // wire apu_wr_signal_RVdomain = (data_write_n == 2'b10) ? 1'b1 :     
-    //                               (data_write_n == 2'b01) ? 1'b1 :     
-    //                               (data_write_n == 2'b00) ? 1'b1 :     
-    //                               1'b0;                    
-
-    // wire apu_rw_signal_RVdomain = (data_read_n == 2'b10) ? 1'b1 :     
-    //                               (data_read_n == 2'b01) ? 1'b1 :     
-    //                               (data_read_n == 2'b00) ? 1'b1 :     
-    //                               1'b0;                     
-
-    // wire apu_rw = (apu_wr_signal_RVdomain) ? 1'b0 : 1'b1;
-    
-    // // The APU module should be modified to take a clock enable,
-    // // rather than the derived clock signal.
-    // APU apu(
-    //     .MMC5(apu_is_mmc5),
-    //     .clk(clk),
-    //     .PHI2(apu_phi2_ce), // Pass the clock enable to PHI2
-    //     .ce(apu_ce),
-    //     .reset(~rst_n),
-    //     .cold_reset(~rst_n),
-    //     .allow_us(apu_us),
-    //     .ADDR(address[4:0]),
-    //     .DIN(data_in[7:0]),
-    //     .RW(apu_rw), 
-    //     .CS(apu_cs),
-    //     .audio_channels(apu_audio_channels),
-    //     .DmaData(),       
-    //     .odd_or_even(odd_or_even),
-    //     .DmaAck(),        
-    //     .DOUT(apu_dout),
-    //     .Sample(apu_output_sample_16b),
-    //     .DmaReq(),        
-    //     .DmaAddr(),       
-    //     .IRQ(apu_IRQ),
-    //     .o_ce(apu_o_ce)
-    // );
-
-    // Correctly capture the APU sample when `o_ce` is high.
-    always @(posedge clk) begin
-        if(!rst_n) begin
-            reg_data_output_msb <= 8'h00;
-            reg_data_output_lsb <= 8'h00;
-        end else if (apu_o_ce) begin
-            reg_data_output_msb <= apu_output_sample_16b[15:8];
-            reg_data_output_lsb <= apu_output_sample_16b[7:0];
-        end
-    end
-    
-    assign uo_out[7:5] = ui_in[7:5];
-    assign uo_out[4]   = apu_IRQ;
-    assign uo_out[3]   = apu_phi2_clk; // The output pin is still the derived clock
-    assign uo_out[2:0] = ui_in[2:0];                
-
-    integer i;
-    always_ff @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            for (i = 0; i <= 30; i = i + 1) begin
-                reg_apu[i] <= 8'h00;
-            end
-        end else begin
-            if (data_write_n == 2'b00) begin
-                if (address >= 6'h00 && address < 6'h20) begin
-                    reg_apu[address[4:0]] <= data_in[7:0];
-                end
-            end
-        end
-    end
-    
+    // Implement a 32-bit read/write register at address 0
+    reg [31:0] example_data;
     always @(posedge clk) begin
         if (!rst_n) begin
-            reg_configuration0 <= 0;
+            example_data <= 0;
         end else begin
-            if (address == CONFIGURATION0_REG_ADDR[5:0]) begin
-                if (data_write_n != 2'b11)
-                    reg_configuration0 <= data_in[7:0];
+            if (address == 6'h0) begin
+                if (data_write_n != 2'b11)              example_data[7:0]   <= data_in[7:0];
+                if (data_write_n[1] != data_write_n[0]) example_data[15:8]  <= data_in[15:8];
+                if (data_write_n == 2'b10)              example_data[31:16] <= data_in[31:16];
             end
         end
     end
 
-    always @(posedge clk) begin
-        if (!rst_n) begin
-            reg_configuration1 <= 0;
-        end else begin
-            if (address == CONFIGURATION1_REG_ADDR[5:0]) begin
-                if (data_write_n != 2'b11)
-                    reg_configuration1 <= data_in[7:0];
-            end
-        end
-    end
+    // The bottom 8 bits of the stored data are added to ui_in and output to uo_out.
+    assign uo_out = example_data[7:0] + ui_in;
 
-    always @(posedge clk) begin
-        if (!rst_n) begin
-            reg_data_input <= 0;
-        end else begin
-            if (address == DATA_INPUT_REG_ADDR) begin
-                if (data_write_n != 2'b11)
-                    reg_data_input <= data_in[7:0];
-            end
-        end
-    end
+    // Address 0 reads the example data register.  
+    // Address 4 reads ui_in
+    // All other addresses read 0.
+    assign data_out = (address == 6'h0) ? example_data :
+                      (address == 6'h4) ? {24'h0, ui_in} :
+                      32'h0;
 
-    reg [31:0] data_out_reg;
-    always_comb begin
-        case (address)
-            6'h00: data_out_reg = {24'h0, reg_apu[0]};
-            CONFIGURATION0_REG_ADDR: data_out_reg = {24'h0, reg_configuration0};
-            CONFIGURATION1_REG_ADDR: data_out_reg = {24'h0, reg_configuration1};
-            STATUS1_REG_ADDR: data_out_reg = {24'h0, reg_status0};
-            DATA_INPUT_REG_ADDR: data_out_reg = {24'h0, reg_data_input};
-            DATA_OUTPUT_MSB_REG_ADDR: data_out_reg = {24'h0, reg_data_output_msb};
-            DATA_OUTPUT_LSB_REG_ADDR: data_out_reg = {24'h0, reg_data_output_lsb};
-            APU_STATUS_REG_ADDRESS: data_out_reg = {24'h0, apu_dout};
-            APU_FRAME_COUNTER_REG_ADDRESS: data_out_reg = {24'h0, apu_dout};
-            default: begin
-                if (address >= 6'h00 && address < 6'h20) begin
-                    data_out_reg = {24'h0, reg_apu[address]};
-                end else begin
-                    data_out_reg = 32'h0;
-                end
-            end
-        endcase
-    end
-    assign data_out = data_out_reg;
-
+    // All reads complete in 1 clock
     assign data_ready = 1;
     
+    // User interrupt is generated on rising edge of ui_in[6], and cleared by writing a 1 to the low bit of address 8.
     reg example_interrupt;
     reg last_ui_in_6;
 
@@ -243,11 +72,15 @@ module tqvp_fjpolo_rv2a03 (
         end else if (address == 6'h8 && data_write_n != 2'b11 && data_in[0]) begin
             example_interrupt <= 0;
         end
+
         last_ui_in_6 <= ui_in[6];
     end
 
     assign user_interrupt = example_interrupt;
 
+    // List all unused inputs to prevent warnings
+    // data_read_n is unused as none of our behaviour depends on whether
+    // registers are being read.
     wire _unused = &{data_read_n, 1'b0};
 
 endmodule
